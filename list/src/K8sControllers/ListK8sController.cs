571a897750904a857481c9192bfe4ae9a8415f37
lknite <lknite@aarr.xyz>
2023-06-17 15:32:12 -0600

    catch list delete and delete associated blocks

--- list/src/K8sControllers/ListK8sController.cs
+++ list/src/K8sControllers/ListK8sController.cs
@@ -4,6 +4,8 @@ using k8s;
 using System.Text.Json;
 using list.crd.list;
 using list.K8sHelpers;
+using list.crd.block;
+using list.CustomResourceDefinitions;
 
 namespace gge.K8sControllers
 {
@@ -94,284 +96,22 @@ namespace gge.K8sControllers
             Console.WriteLine("Addition/Modify detected: " + l.Metadata.Name);
             Console.WriteLine("g.Spec.state: " + l.Spec.list.state);
 
-            /*
-            // only work with new lists
-            if (l.Spec.invite.state != "")
+
+            // loop through all blocks associated with deleted list & delete
+            CustomResourceList<CrdBlock> blocks = await zK8sBlock.generic.ListNamespacedAsync<CustomResourceList<CrdBlock>>(
+                    Globals.service.kubeconfig.Namespace);
+            foreach (CrdBlock block in blocks.Items)
             {
-                return;
-            }
-            */
-
-            //
-            switch (l.Spec.list.state)
-            {
-                /*
-                case "accepted":
-                    Console.WriteLine("processing 'accepted'");
-
-                    // form data to append to event
-                    data = new Dictionary<string, string>();
-                    data.Add("email", i.Spec.invite.email);
-
-                    // invite was accepted, generate event
-                    await zK8sEvent.Post(
-                        (DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond).ToString(),
-                        "",
-                        0,
-                        0,
-                        "invite-accepted",
-                        JsonSerializer.Serialize(data),
-                        i.Spec.parent.template,
-                        i.Spec.parent.game,
-                        false
-                        );
-
-                    //
-                    CrdGame g = await Globals.service.generic.ReadNamespacedAsync<CrdGame>("gge", i.Spec.parent.game.ToString());
-
-                    // new player, generate event
-                    Console.WriteLine("add player to game");
-                    if (!i.Spec.invite.is_watcher)
-                    {
-                        // Add player to game
-                        if (g.Spec.players == null)
-                        {
-                            g.Spec.players = new List<Player>();
-                        }
-                        // First make sure user is not already a player
-                        bool found = false;
-                        foreach (Player p in g.Spec.players)
-                        {
-                            if (p.email.Equals(i.Spec.invite.email))
-                            {
-                                found = true;
-                                break;
-                            }
-                        }
-                        if (found)
-                        {
-                            // possible race condition
-                            break;
-                        }
-                        // Player not found, go ahead and add
-                        g.Spec.players.Add(new Player()
-                        {
-                            user = i.Spec.invite.user,
-                            email = i.Spec.invite.email,
-                            algo = i.Spec.invite.algo,
-                            id = g.Spec.players.Count + 1
-                        });
-
-                        // form data to append to event
-                        data = new Dictionary<string, string>();
-                        data.Add("email", i.Spec.invite.email);
-
-                        // player was added, generate event
-                        await zK8sEvent.Post(
-                            (DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond).ToString(),
-                            "",
-                            0,
-                            0,
-                            "player-add",
-                            JsonSerializer.Serialize(data),
-                            i.Spec.parent.template,
-                            i.Spec.parent.game,
-                            false
-                            );
-                    }
-                    else
-                    {
-                        // Add watcher to game
-                        if (g.Spec.watchers == null)
-                        {
-                            g.Spec.watchers = new List<Player>();
-                        }
-                        // First make sure user is not already a player
-                        bool found = false;
-                        foreach (Player p in g.Spec.watchers)
-                        {
-                            if (p.email.Equals(i.Spec.invite.email))
-                            {
-                                found = true;
-                                break;
-                            }
-                        }
-                        if (found)
-                        {
-                            // possible race condition
-                            break;
-                        }
-                        // Watcher not found, go ahead and add
-                        g.Spec.watchers.Add(new Player() {
-                            user = i.Spec.invite.user,
-                            email = i.Spec.invite.email,
-                            algo = i.Spec.invite.algo,
-                            id = g.Spec.watchers.Count + 1
-                        });
-                    }
-
-                    // save the players and watchers before we save the invite as complete
-                    patchStr = "{ \"spec\": {"
-                                    + "\"players\": "
-                                    + JsonSerializer.Serialize(g.Spec.players, new JsonSerializerOptions { WriteIndented = true })
-                                    + ","
-                                    + "\"watchers\": "
-                                    + JsonSerializer.Serialize(g.Spec.watchers, new JsonSerializerOptions { WriteIndented = true })
-                                    + "} }";
-                    await Globals.service.generic.PatchNamespacedAsync<CrdGame>(
-                            new V1Patch(patchStr, V1Patch.PatchType.MergePatch), "gge", g.Metadata.Name);
-
-                    // Do we have two players?
-                    if (g.Spec.players == null)
-                    {
-                        g.Spec.players = new List<Player>();
-                    }
-                    if (g.Spec.players.Count != 2)
-                    {
-                        // We do not have two players, don't start the game yet
-
-                        // set state
-                        i.Spec.invite.state = "complete";
-
-                        break;
-                    }
-
-                    // Start game, we have our two players
-
-                    // form data to append to event
-                    data = new Dictionary<string, string>();
-
-                    // game-staging, generate event
-                    await zK8sEvent.Post(
-                        (DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond).ToString(),
-                        "",
-                        0,
-                        0,
-                        "game-staging",
-                        JsonSerializer.Serialize(data),
-                        i.Spec.parent.template,
-                        i.Spec.parent.game,
-                        false
-                        );
-
-                    // Let players know what their player id is
-                    foreach (Player player in g.Spec.players)
-                    {
-                        // form data to append to event
-                        Dictionary<string, int> dataPlayerId = new Dictionary<string, int>();
-                        dataPlayerId.Add("id", player.id);
-
-                        // game-staging, generate event
-                        await zK8sEvent.Post(
-                            (DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond).ToString(),
-                            "",
-                            0,
-                            player.id,
-                            "player-id",
-                            JsonSerializer.Serialize(dataPlayerId),
-                            i.Spec.parent.template,
-                            i.Spec.parent.game,
-                            false
-                            );
-                    }
-
-
-                    // set turn
-                    g.Spec.turn = 1;
-
-                    // set state
-                    g.Spec.state = "active";
-
-                    // save the turn counter and the new state of the game
-                    patchStr = "{ \"spec\": {"
-                                    + "\"turn\": "
-                                    + g.Spec.turn
-                                    + ","
-                                    + "\"state\": "
-                                    + "\"" + g.Spec.state + "\""
-                                    + "} }";
-                    await Globals.service.generic.PatchNamespacedAsync<CrdGame>(
-                            new V1Patch(patchStr, V1Patch.PatchType.MergePatch), "gge", g.Metadata.Name);
-
-                    // form data to append to event
-                    data = new Dictionary<string, string>();
-
-                    // game-staging, generate event
-                    await zK8sEvent.Post(
-                        (DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond).ToString(),
-                        "",
-                        0,
-                        0,
-                        "game-start",
-                        JsonSerializer.Serialize(data),
-                        i.Spec.parent.template,
-                        i.Spec.parent.game,
-                        false
-                        );
-
-                    // form data to append to event
-                    Dictionary<string, int> dataTurn = new Dictionary<string, int>();
-                    dataTurn.Add("id", g.Spec.turn);
-
-                    // game-staging, generate event
-                    await zK8sEvent.Post(
-                        (DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond).ToString(),
-                        "",
-                        0,
-                        0,
-                        "turn",
-                        JsonSerializer.Serialize(dataTurn),
-                        i.Spec.parent.template,
-                        i.Spec.parent.game,
-                        false
-                        );
-
-                    // set state
-                    i.Spec.invite.state = "complete";
-
-                    break;
-                case "rejected":
-                    Console.WriteLine("processing 'rejected'");
-
-                    // form data to append to event
-                    data = new Dictionary<string, string>();
-                    data.Add("email", i.Spec.invite.email);
-
-                    // invite was rejected, generate event
-                    await zK8sEvent.Post(
-                        (DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond).ToString(),
-                        "",
-                        0,
-                        0,
-                        "invite-rejected",
-                        JsonSerializer.Serialize(data),
-                        i.Spec.parent.template,
-                        i.Spec.parent.game,
-                        false
-                        );
-
-                    // set state
-                    i.Spec.invite.state = "complete";
-
-                    break;
-                */
+                // is this block associated with the list?
+                if (block.Spec.block.list.Equals(l.Metadata.Name))
+                {
+                    // if yes, then delete block 
+                    zK8sBlock.generic.DeleteNamespacedAsync<CrdBlock>(
+                            Globals.service.kubeconfig.Namespace,
+                            block.Metadata.Name);
+                }
             }
 
-            /*
-            // if we finished processing the invite go ahead and mark it as complete
-            // otherwise, in the case of an exception the invite will be processed again
-            if (i.Spec.invite.state == "complete")
-            {
-                //
-                patchStr = "{ \"spec\": { \"invite\": "
-                    + JsonSerializer.Serialize(i.Spec.invite, new JsonSerializerOptions { WriteIndented = true })
-                    + "} }";
-
-                //Console.WriteLine("patchStr:\n" + patchStr);
-                await generic.PatchNamespacedAsync<crd.invite.CrdInvite>(
-                    new V1Patch(patchStr, V1Patch.PatchType.MergePatch), "gge", i.Metadata.Name);
-            }
-            */
 
             return;
         }
