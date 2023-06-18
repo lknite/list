4f03e46f549376b3f36a8d06254d322ca2b1f40b
lknite <lknite@aarr.xyz>
2023-06-18 06:58:13 -0600

    add list lookup to message method

--- list/src/Controllers/MessageController.cs
+++ list/src/Controllers/MessageController.cs
@@ -1,4 +1,7 @@
+using list.crd.list;
+using list.K8sHelpers;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 using System.Security.Claims;
 
 namespace list.Controllers
@@ -8,20 +11,6 @@ namespace list.Controllers
     [Produces("application/json")]
     public class MessageController : ControllerBase
     {
-        /*
-        /// <summary>
-        /// </summary>
-        /// <returns></returns>
-        [HttpGet()]
-        public async Task<IActionResult> Get()
-        {
-            Console.WriteLine("Username: " + User.FindFirstValue(Environment.GetEnvironmentVariable("OIDC_USER_CLAIM")));
-            Console.WriteLine("Email: " + User.FindFirstValue("email"));
-
-            return Ok();
-        }
-        */
-
         /// <summary>
         ///  send list owner a notification
         /// </summary>
@@ -41,6 +30,35 @@ namespace list.Controllers
             // debug
             Console.WriteLine("Username: " + User.FindFirstValue(Environment.GetEnvironmentVariable("OIDC_USER_CLAIM")));
             Console.WriteLine("Email: " + User.FindFirstValue("email"));
+
+            CrdList l = null;
+            try
+            {
+                // look up list
+                l = await zK8sList.generic.ReadNamespacedAsync<CrdList>(Globals.service.kubeconfig.Namespace, list);
+                if (!l.Spec.list.owner.Equals(User.FindFirstValue(Environment.GetEnvironmentVariable("OIDC_USER_CLAIM"))))
+                {
+                    return StatusCode(StatusCodes.Status403Forbidden);
+                }
+
+                // only someone allowed to participate in a list is allowed to send messages
+            }
+            catch (k8s.Autorest.HttpOperationException ex)
+            {
+                /*
+                Console.WriteLine("** one **");
+                */
+                Console.WriteLine("StatusCode: " + ex.Response.StatusCode);
+                /*
+                Console.WriteLine("   Message: " + ex.Message);
+                Console.WriteLine("      Data: " + ex.InnerException.Data);
+                */
+
+                if (ex.Response.StatusCode == HttpStatusCode.NotFound)
+                {
+                    return StatusCode(StatusCodes.Status404NotFound);
+                }
+            }
 
 
             return Ok();
